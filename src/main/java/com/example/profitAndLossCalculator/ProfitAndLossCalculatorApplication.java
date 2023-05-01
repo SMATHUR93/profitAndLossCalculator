a1af629739f23d7ee1039e55e5466fc328ab070b
smathur93 <shreymathur93@gmail.com>
2023-05-01 15:02:05 +0530

    Phase 1

--- src/main/java/com/example/profitAndLossCalculator/ProfitAndLossCalculatorApplication.java
+++ src/main/java/com/example/profitAndLossCalculator/ProfitAndLossCalculatorApplication.java
@@ -2,8 +2,9 @@ package com.example.profitAndLossCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ProfitAndLossCalculatorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/profitAndLossCalculator/controller/ProfitAndLossController.java
+++ src/main/java/com/example/profitAndLossCalculator/controller/ProfitAndLossController.java
@@ -2,10 +2,7 @@ package com.example.profitAndLossCalculator.controller;
 
 import com.example.profitAndLossCalculator.service.ProfitAndLossService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profit-and-loss")
@@ -14,7 +11,7 @@ public class ProfitAndLossController {
     @Autowired
     private ProfitAndLossService profitAndLossService;
 
-    @PostMapping
+    @GetMapping
     public Double calculateProfitAndLoss(@RequestParam Double costPrice, @RequestParam Double sellingPrice) {
         return profitAndLossService.calculateProfitAndLoss(costPrice, sellingPrice);
     }
